9a6f4ecde353bae50920cc9c594f9495826febda
slaven <slaven.susic23@gmail.com>
2023-08-17 11:23:16 +0200

    sec

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +10,6 @@ export default defineConfig({
   experimental: {
     viewTransitions: true
   },
-  output: "server",
-  adapter: vercel()
+  // output: "server",
+  // adapter: vercel()
 });
